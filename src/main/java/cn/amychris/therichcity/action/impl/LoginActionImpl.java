063851e4f6b466100828b8079f2f16c626d8adb8
amy zhang <zhangyanxia14@163.com>
2012-06-16 20:26:13 +0800

    the new code

--- src/main/java/cn/amychris/therichcity/action/impl/LoginActionImpl.java
+++ src/main/java/cn/amychris/therichcity/action/impl/LoginActionImpl.java
@@ -2,11 +2,6 @@ package cn.amychris.therichcity.action.impl;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
-
 import cn.amychris.therichcity.action.LoginAction;
 import cn.amychris.therichcity.entity.UserEntity;
 import cn.amychris.therichcity.exception.SecurityServiceException;
@@ -15,45 +10,46 @@ import cn.amychris.therichcity.service.LoginService;
 import cn.amychris.therichcity.service.UserService;
 import cn.amychris.therichcity.validator.UserFormValidator;
 
-@Scope ("prototype")
-@Controller ("loginAction")
 public class LoginActionImpl implements LoginAction {
 
-	@Autowired
-	@Qualifier("loginService")
 	private LoginService loginService;
 
-	@Autowired
-	@Qualifier("userService")
 	private UserService userService;
 
-	@Autowired
-	@Qualifier("userFormLoginValidor")
-	private UserFormValidator userFormLoginValidor;
+	private UserFormValidator userFormLoginValidator;
 
 	private List<String> errorMsgs;
-	
+
 	private UserForm userForm = new UserForm();
 
 	@Override
 	public String login () {
-		errorMsgs = userFormLoginValidor.validate(userForm);
-		if ( !errorMsgs.isEmpty() ) {
-			return "loginFailed";
-		}
-		UserEntity user = userService.getByEmail(userForm.getEmail());
-		if ( !user.getPassword().equals(userForm.getPassword()) ) {
-			errorMsgs.add("√‹¬Î¥ÌŒÛ°£");
-			return "loginFailed";
-		}
-		try {
-			loginService.login(user);
-		} catch ( SecurityServiceException e ) {
-			errorMsgs.add(e.getLocalizedMessage());
-			return "loginFailed";
+		errorMsgs = userFormLoginValidator.validate( userForm );
+		
+		if ( ! errorMsgs.isEmpty() ) {
+			return "Login Unsuccessfully";
 		}
 
-		return "success";
+		UserEntity user = userService.getByEmail( userForm.getEmail() );
+
+		if ( null == user ) {
+			errorMsgs.add( "” œ‰°æ" + userForm.getEmail() + "°øŒ¥◊¢≤·" );
+			return "Login Unsuccessfully";
+		}
+
+		if ( ! user.getPassword().equals( userForm.getPassword() ) ) {
+			errorMsgs.add( "√‹¬Î¥ÌŒÛ°£" );
+			return "Login Unsuccessfully";
+		}
+
+		try {
+			loginService.login( user );
+		} catch ( SecurityServiceException e ) {
+			errorMsgs.add( e.getLocalizedMessage() );
+			return "Login Unsuccessfully";
+		}
+
+		return "Login Successfully";
 	}
 
 	@Override
@@ -71,11 +67,21 @@ public class LoginActionImpl implements LoginAction {
 	public List<String> getErrorMsgs () {
 		return errorMsgs;
 	}
-	
+
 	public UserForm getUserForm () {
 		return userForm;
 	}
-	
-	
+
+	public void setLoginService ( LoginService loginService ) {
+		this.loginService = loginService;
+	}
+
+	public void setUserService ( UserService userService ) {
+		this.userService = userService;
+	}
+
+	public void setUserFormLoginValidator ( UserFormValidator userFormLoginValidator ) {
+		this.userFormLoginValidator = userFormLoginValidator;
+	}
 
 }

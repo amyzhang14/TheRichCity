f9b4e482b764ab20a4957ffd0cdb8b312ef1bcee
Christian Chen <328427972@qq.com>
2012-07-23 23:16:49 +0800

    added jquery

---
+++ src/main/java/cn/amychris/therichcity/action/impl/GameHallActionImpl.java
@@ -0,0 +1,9 @@
+package cn.amychris.therichcity.action.impl;
+
+import cn.amychris.therichcity.action.GameHallAction;
+import cn.amychris.therichcity.form.UserForm;
+
+public class GameHallActionImpl extends BaseActionImpl implements GameHallAction {
+
+	
+}
